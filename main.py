5199257db37c5977a7c80ce58f0378d9f8546d49
Marco Resendiz <113551818+maresendiz2@users.noreply.github.com>
2022-10-03 19:20:00 +0000

    made functions from files

--- main.py
+++ main.py
@@ -11,7 +11,25 @@
 
 from decision_making import decisions
 from comparison_operators import comparison
-
+from dictionaries_loops import dictionaries_loops
+from enumerators import enumerators
+from list_comprehension import list_comprehension
+from logical_operators import logical_operators
+from loops import loops
+from min_max import min_max
+from random import random
+from ranges import ranges
+from while_loops import while_loops
+from zip import zip
 decisions()
 comparison()
-
+dictionaries_loops()
+enumerators()
+list_comprehension()
+logical_operators()
+loops()
+min_max()
+random()
+ranges()
+while_loops()
+zip()
